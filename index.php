f950ae05c46fe6cde84a31a990ea67a32c876fb3
Martin O'Sullivan <osumartin@gmail.com>
2017-11-03 21:51:43 +0000

    updated files

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE HTML>  
 <html lang="en">
         <head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
         	<meta charset="UTF-8">
         	<link rel="stylesheet" href="css/stylesheet.css">
         	<script type="text/javascript" src="js/jquery.min.js"></script>
