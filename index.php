48386cbaae7f11debdc632ef108e6a5d440b6337
Martin O'Sullivan <osumartin@gmail.com>
2017-11-13 22:44:31 +0000

    updated the layout of the site.

--- index.php
+++ index.php
@@ -74,8 +74,11 @@ function test_input($data) {
 }
 ?>
 
-<h1>Test A Password</h1>
-
+<p>
+<div class="main-logo">
+<span class="logo">Is this password secure?</span>
+</div>
+</p>
 <form method="post" action="index.php">
 <label for="password"></label> 
 <input type="password" name="password" id="fmPass" autofocus placeholder="Enter The Password You Want To Test" value="">
@@ -104,13 +107,6 @@ function test_input($data) {
     $my_display->display_results($hack_details);
   } // end if Method == POST
 ?>
-
-  <p>you can use This  strength meter when you're creating an account on any site. It will tell  you how many possible passwords there are based on the length and character classes used.</p>
-  <p>For example, the password "1p" has 2 characters, one  a lowercase letter and one a number.  To brute force this password, there are 26 possible letters and 10 possible numbers which is (10 + 26) * (10 + 26) = 1,296 possibilities
-    . Less than a second to crack!
-  </p>
-  <p>These numbers are only for demonstration purposes.  Please do your own research on password security!  The numbers are derived from <a href="http://hashcat.net/oclhashcat-plus/">hashcat's site</a>.</p>
-  <br />
 
    </div>
   <!-- End body text -->
